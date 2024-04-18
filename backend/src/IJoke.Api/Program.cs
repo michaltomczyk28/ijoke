c1ac0ba9fa289f2db97db80adcf81000f5eb7d14
Michał Tomczyk <99367820+michaltomczyk28@users.noreply.github.com>
2024-04-18 20:04:57 +0200

    fix: adjust cors policy to allow requests from port 5173 (#9)

--- backend/src/IJoke.Api/Program.cs
+++ backend/src/IJoke.Api/Program.cs
@@ -10,12 +10,25 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<JokeDbContext>(
     options => options.UseNpgsql("name=ConnectionStrings:JokeCS"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendDevClient",
+        builder =>
+        {
+            builder
+                .WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IJokeRepository, JokeRepository>();
 builder.Services.AddScoped<IJokeService, JokeService>();
 
 var app = builder.Build();
 
+app.UseCors("AllowFrontendDevClient");
 app.UseHttpsRedirection();
 app.UseRouting();
 
